fdce3ad493c20cb375a16783af1cd1890671de9c
Jason Jackson <jake1164@hotmail.com>
2022-12-02 15:48:48 -0500

    renamed to make more obvious

--- demo1 - icon display/3 - rp2040.py
+++ demo1 - icon display/3 - rp2040.py
@@ -8,18 +8,8 @@ import adafruit_imageload
 import terminalio
 from adafruit_display_text.label import Label
 from digitalio import DigitalInOut, Direction
-
-bit_depth_value = 1
-base_width = 64
-base_height = 32
-chain_across = 2
-tile_down = 1
-serpentine = True
-
-width_value = base_width * chain_across
-height_value = base_height * tile_down
-
 displayio.release_displays()
+
 # send register
 R1 = DigitalInOut(board.GP2)
 G1 = DigitalInOut(board.GP3)
@@ -113,26 +103,22 @@ STB.deinit()
 OE.deinit()
 
 matrix = rgbmatrix.RGBMatrix(
-    width=width_value, height=height_value, bit_depth=bit_depth_value,
+    width=64, bit_depth=6,
     rgb_pins=[board.GP2, board.GP3, board.GP4, board.GP5, board.GP8, board.GP9],
     addr_pins=[board.GP10, board.GP16, board.GP18, board.GP20],
     clock_pin=board.GP11, latch_pin=board.GP12, output_enable_pin=board.GP13,
-    tile=tile_down, serpentine=serpentine,
     doublebuffer=True)
-display = framebufferio.FramebufferDisplay(matrix, auto_refresh=False)
+display = framebufferio.FramebufferDisplay(matrix, auto_refresh=False,rotation=180)
 
-display.rotation = 180
+
 
 g = displayio.Group()
-b, p = adafruit_imageload.load("images/ws.bmp")
-#b, p = adafruit_imageload.load("pi-logo32b.bmp")
-#b, p = adafruit_imageload.load("wales_32x64.bmp")
+b, p = adafruit_imageload.load("images/pi-logo32b.bmp")
 t = displayio.TileGrid(b, pixel_shader=p)
-t.x = 15
-#t.x = 0
+t.x = 20
 g.append(t)
 
-l = Label(text="Waveshare\nRGB Matrix", font=terminalio.FONT, color=0xffffff, line_spacing=.7)
+l = Label(text="Feather\nRP2040", font=terminalio.FONT, color=0xffffff, line_spacing=.7)
 g.append(l)
 
 display.show(g)
--- demo1 - icon display/4 - waveshare.py
+++ demo1 - icon display/4 - waveshare.py
@@ -1,27 +1,25 @@
-# Minimal example displaying an image tiled across multiple RGB LED matrices.
-# This is written for MatrixPortal and four 64x32 pixel matrices, but could
-# be adapted to different boards and matrix combinations.
-# No additional libraries required, just uses displayio.
-# Image word.bmp should be in CIRCUITPY images directory.
-
+import time
+from math import sin
 import board
 import displayio
-import framebufferio
 import rgbmatrix
+import framebufferio
+import adafruit_imageload
+import terminalio
+from adafruit_display_text.label import Label
 from digitalio import DigitalInOut, Direction
 
-bit_depth_value = 6
+bit_depth_value = 1
 base_width = 64
 base_height = 32
-chain_across = 1
+chain_across = 2
 tile_down = 1
-serpentine_value = True
+serpentine = True
 
 width_value = base_width * chain_across
 height_value = base_height * tile_down
 
-displayio.release_displays() # Release current display, we'll create our own
-
+displayio.release_displays()
 # send register
 R1 = DigitalInOut(board.GP2)
 G1 = DigitalInOut(board.GP3)
@@ -114,42 +112,50 @@ CLK.deinit()
 STB.deinit()
 OE.deinit()
 
-
-# Create RGB matrix object for a chain of four 64x32 matrices tiled into
-# a single 128x64 pixel display -- two matrices across, two down, with the
-# second row being flipped. width and height args are the combined size of
-# all the tiled sub-matrices. tile arg is the number of rows of matrices in
-# the chain (horizontal tiling is implicit from the width argument, doesn't
-# need to be specified, but vertical tiling must be explicitly stated).
-# The serpentine argument indicates whether alternate rows are flipped --
-# cabling is easier this way, downside is colors may be slightly different
-# when viewed off-angle. bit_depth and pins are same as other examples.
 matrix = rgbmatrix.RGBMatrix(
     width=width_value, height=height_value, bit_depth=bit_depth_value,
     rgb_pins=[board.GP2, board.GP3, board.GP4, board.GP5, board.GP8, board.GP9],
     addr_pins=[board.GP10, board.GP16, board.GP18, board.GP20],
     clock_pin=board.GP11, latch_pin=board.GP12, output_enable_pin=board.GP13,
-    tile=tile_down, serpentine=serpentine_value,
+    tile=tile_down, serpentine=serpentine,
     doublebuffer=True)
+display = framebufferio.FramebufferDisplay(matrix, auto_refresh=False)
 
-# Associate matrix with a Display to use displayio features
-DISPLAY = framebufferio.FramebufferDisplay(matrix, auto_refresh=False,
-                                           rotation=180)
+display.rotation = 180
 
-# Load BMP image, create Group and TileGrid to hold it
-BITMAP = displayio.OnDiskBitmap(open('images/Word.bmp', 'rb'))
-GROUP = displayio.Group()
-GROUP.append(displayio.TileGrid(
-    BITMAP,
-    pixel_shader=getattr(BITMAP, 'pixel_shader', displayio.ColorConverter()),
-    width=1,
-    height=1,
-    tile_width=BITMAP.width,
-    tile_height=BITMAP.height))
-DISPLAY.show(GROUP)
-DISPLAY.refresh()
+g = displayio.Group()
+b, p = adafruit_imageload.load("images/ws.bmp")
+#b, p = adafruit_imageload.load("pi-logo32b.bmp")
+#b, p = adafruit_imageload.load("wales_32x64.bmp")
+t = displayio.TileGrid(b, pixel_shader=p)
+t.x = 15
+#t.x = 0
+g.append(t)
 
-# Nothing interactive, just hold the image there
+l = Label(text="Waveshare\nRGB Matrix", font=terminalio.FONT, color=0xffffff, line_spacing=.7)
+g.append(l)
+
+display.show(g)
+
+target_fps = 50
+ft = 1/target_fps
+now = t0 = time.monotonic_ns()
+deadline = t0 + ft
+
+p = 1
+q = 17
 while True:
-    pass
-
+    tm = (now - t0) * 1e-9
+    x = l.x - 1
+    if x < -40:
+        x = 63
+    y =  round(12 + sin(tm / p) * 6)
+    l.x = x
+    l.y = y
+    display.refresh(target_frames_per_second=target_fps, minimum_frames_per_second=0)
+    while True:
+        now = time.monotonic_ns()
+        if now > deadline:
+            break
+        time.sleep((deadline - now) * 1e-9)
+    deadline += ft
